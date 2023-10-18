78a0578d5bf4f865dfb8371c5c54eafdced2f838
negruvalentin94 <negruvalentin94@yahoo.com>
2023-10-18 20:00:20 +0300

    Initial version

---
+++ main.py
@@ -0,0 +1,19 @@
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl("https://universe.flyff.com/play"))
+        self.setCentralWidget(self.browser)
+
+# This is needed on Linux manjaro for URL to load
+app = QApplication(['', '--no-sandbox'])
+window = MainWindow()
+window.setWindowTitle('FlyffHarcore')
+window.show()
+
+app.exec_()
