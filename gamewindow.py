b7ccd8177f6d39e204328a7e8871bb8a53baa99c
vnegru <negruvalentin94@yahoo.com>
2023-10-24 21:35:15 +0300

    file read from folder, overwatch function added, to be added to a thread

--- gamewindow.py
+++ gamewindow.py
@@ -1,6 +1,14 @@
-from PyQt5.QtCore import QUrl
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtCore import QUrl, pyqtSignal, QObject
+from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox, QAction
 from PyQt5.QtWebEngineWidgets import QWebEngineView
+from main import overwatch
+
+class PopUpDialog(QMessageBox):
+    def __init__(self):
+        super(PopUpDialog, self).__init__()
+        self.setWindowTitle("Popup Dialog")
+        self.setText("This is a pop-up dialog.")
+        self.addButton(QMessageBox.Ok)
 
 class WebBrowser(QMainWindow):
     def __init__(self):
@@ -10,9 +18,21 @@ class WebBrowser(QMainWindow):
         self.browser.setUrl(QUrl("https://universe.flyff.com/play"))
         self.setCentralWidget(self.browser)
 
+        self.ftool = QAction("Mini FTool", self)
+        self.ftool.triggered.connect(self.show_popup)
+
+    def show_popup(self):
+        # Create and show the pop-up dialog
+        pop_up = PopUpDialog()
+        pop_up.exec_()
+
+
 app = QApplication([])
 window = WebBrowser()
 window.setWindowTitle('FlyffHarcore')
 window.show()
+a = overwatch()
+if a:
+    window.addAction(window.ftool)
 app.exec_()
 
--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 from time import time
 from windowcapture import WindowCapture
 from vision import Vision
-import cv2 as cv
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox
+import os
 
-# Your main script logic here
-wincap = WindowCapture('FlyffHarcore')
-# initialize the Vision class
-vision_limestone = Vision('new_char_gold.PNG')
+script_directory = os.path.dirname(os.path.abspath(__file__))
 
-loop_time = time()
-wincap.start()
-while (True):
+image_path = os.path.join(script_directory, 'images', 'new_char_original.PNG')
 
-    # if we don't have a screenshot yet, don't run the code below this point yet
-    if wincap.screenshot is None:
-        continue
+def overwatch():
+    # Your main script logic here
+    wincap = WindowCapture('FlyffHarcore')
+    # initialize the Vision class
+    vision_limestone = Vision(image_path)
 
-    # display the processed image
-    points = vision_limestone.find(wincap.screenshot)
-
-    # debug the loop rate
-    print('FPS {}'.format(1 / (time() - loop_time)))
     loop_time = time()
+    wincap.start()
+    while (True):
 
+        # if we don't have a screenshot yet, don't run the code below this point yet
+        if wincap.screenshot is None:
+            continue
+
+        # display the processed image
+        points = vision_limestone.find(wincap.screenshot)
+
+        # debug the loop rate
+        print('FPS {}'.format(1 / (time() - loop_time)))
+        loop_time = time()
+        if points:
+            break
+
+    return True
