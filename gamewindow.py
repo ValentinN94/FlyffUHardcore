4d133aacbea7e8cc57daae771c7bb25b8b295d53
vnegru <negruvalentin94@yahoo.com>
2023-10-25 20:40:39 +0300

    changes to gamewindow, testing in utils, removed files unused

--- gamewindow.py
+++ gamewindow.py
@@ -1,15 +1,40 @@
-from PyQt5.QtCore import QUrl, pyqtSignal, QObject
-from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox, QAction
+import os
+from time import time
+from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot, QThread, QUrl
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
 from PyQt5.QtWebEngineWidgets import QWebEngineView
-from main import overwatch
+from windowcapture import WindowCapture
+from vision import Vision
 
-class PopUpDialog(QMessageBox):
-    def __init__(self):
-        super(PopUpDialog, self).__init__()
-        self.setWindowTitle("Popup Dialog")
-        self.setText("This is a pop-up dialog.")
-        self.addButton(QMessageBox.Ok)
+# Create a worker class that subclasses QObject for running the 'overwatch' function in a separate thread.
+class OverwatchWorker(QObject):
+    finished = pyqtSignal(bool)
 
+    def run(self):
+        script_directory = os.path.dirname(os.path.abspath(__file__))
+        image_path = os.path.join(script_directory, 'images', 'lvl_1_char.PNG')
+
+        wincap = WindowCapture('FlyffHarcore')
+        new_char = Vision(image_path)
+
+        loop_time = time()
+        wincap.start()
+        while True:
+            if wincap.screenshot is None:
+                continue
+
+            points = new_char.find(wincap.screenshot)
+
+            print('FPS {}'.format(1 / (time() - loop_time)))
+            loop_time = time()
+
+            if points:
+                self.finished.emit(True)
+                return
+
+        self.finished.emit(False)
+
+# Create the main application window using QMainWindow.
 class WebBrowser(QMainWindow):
     def __init__(self):
         super(WebBrowser, self).__init__()
@@ -17,22 +42,51 @@ class WebBrowser(QMainWindow):
         self.browser = QWebEngineView()
         self.browser.setUrl(QUrl("https://universe.flyff.com/play"))
         self.setCentralWidget(self.browser)
+        self.browser.loadFinished.connect(self.on_load_finished)
 
-        self.ftool = QAction("Mini FTool", self)
-        self.ftool.triggered.connect(self.show_popup)
+    # Slot that is called when the web page finishes loading.
+    @pyqtSlot(bool)
+    def on_load_finished(self, ok):
+        if ok:
+            # Create a custom QMessageBox with a "Continue" button
+            msg_box = QMessageBox(self)
+            msg_box.setWindowTitle('Overwatch')
+            msg_box.setText('Your movements will be recorded for the HardcoreUFlyff Challenge. Do you agree?')
 
-    def show_popup(self):
-        # Create and show the pop-up dialog
-        pop_up = PopUpDialog()
-        pop_up.exec_()
+            continue_button = msg_box.addButton('New Character', QMessageBox.AcceptRole)
+            msg_box.addButton(QMessageBox.Yes)
+            msg_box.addButton(QMessageBox.No)
 
+            reply = msg_box.exec()
+
+            if reply == QMessageBox.AcceptRole:
+                self.run_overwatch()
+            else:
+                print("User does not want to continue.")
+        else:
+            print("Page failed to load.")
+
+    # Method to set up and start the new thread for running the 'overwatch' function.
+    def run_overwatch(self):
+        self.overwatch_thread = QThread()
+        self.overwatch_worker = OverwatchWorker()
+        self.overwatch_worker.moveToThread(self.overwatch_thread)
+        self.overwatch_thread.started.connect(self.overwatch_worker.run)
+        self.overwatch_worker.finished.connect(self.on_overwatch_finished)
+        self.overwatch_thread.start()
+
+    # Slot to handle the result from the 'overwatch' function.
+    @pyqtSlot(bool)
+    def on_overwatch_finished(self, success):
+        if success:
+            print("Overwatch finished successfully.")
+        else:
+            print("Overwatch failed.")
+        self.overwatch_thread.quit()
+        self.overwatch_thread.wait()
 
 app = QApplication([])
 window = WebBrowser()
 window.setWindowTitle('FlyffHarcore')
 window.show()
-a = overwatch()
-if a:
-    window.addAction(window.ftool)
 app.exec_()
-
--- utils/utils.py
+++ utils/utils.py
@@ -32,39 +32,74 @@ import cv2
 # this is a matching techniques comparison
 #####################################################################
 
+# import cv2
+# from matplotlib import pyplot as plt
+#
+# img = cv2.imread('../images/screenshoot.PNG', 0)
+# img2 = img.copy()
+# template = cv2.imread('../new_char_gold.PNG', 0)
+# w, h = template.shape[::-1]
+#
+# # All the 6 methods for comparison in a list
+# methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
+#             'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+#
+# for meth in methods:
+#     img = img2.copy()
+#     method = eval(meth)
+#
+#     # Apply template Matching
+#     res = cv2.matchTemplate(img,template,method)
+#     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+#
+#     # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+#     if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+#         top_left = min_loc
+#     else:
+#         top_left = max_loc
+#     bottom_right = (top_left[0] + w, top_left[1] + h)
+#
+#     cv2.rectangle(img,top_left, bottom_right, color=(0, 255, 0), lineType=cv2.LINE_4)
+#
+#     plt.subplot(121),plt.imshow(res,cmap = 'gray')
+#     plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+#     plt.subplot(122),plt.imshow(img,cmap = 'gray')
+#     plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+#     plt.suptitle(meth)
+#
+#     plt.show()
+
 import cv2
 from matplotlib import pyplot as plt
+import os
+from PIL import Image
 
-img = cv2.imread('../images/screenshoot.PNG', 0)
+# script_directory = os.path.dirname(os.path.abspath(__file__))
+
+img = cv2.imread("C:\\Users\\vnegru\\PycharmProjects\\FlyffUHardcore\\utils\\screenshoot_lvl_1.PNG", 0)
 img2 = img.copy()
-template = cv2.imread('../new_char_gold.PNG', 0)
+template = cv2.imread('C:\\Users\\vnegru\\PycharmProjects\\FlyffUHardcore\\utils\\lvl_1_char.PNG', 0)
 w, h = template.shape[::-1]
 
-# All the 6 methods for comparison in a list
-methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
-            'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+img = img2.copy()
 
-for meth in methods:
-    img = img2.copy()
-    method = eval(meth)
 
-    # Apply template Matching
-    res = cv2.matchTemplate(img,template,method)
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+# Apply template Matching
+res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
+min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+top_left = max_loc
+bottom_right = (top_left[0] + w, top_left[1] + h)
 
-    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-    if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
-        top_left = min_loc
-    else:
-        top_left = max_loc
-    bottom_right = (top_left[0] + w, top_left[1] + h)
+#result = cv2.rectangle(img, top_left,bottom_right , color=(0, 255, 255), lineType=cv2.LINE_4)
 
-    cv2.rectangle(img,top_left, bottom_right, color=(0, 255, 0), lineType=cv2.LINE_4)
+x1 = (0, top_left[0])
+x2 = top_left
+y1 = (top_left[0],top_left[1])
+y2 = bottom_right
 
-    plt.subplot(121),plt.imshow(res,cmap = 'gray')
-    plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122),plt.imshow(img,cmap = 'gray')
-    plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-    plt.suptitle(meth)
+result = cv2.rectangle(img, x1,y2 , color=(0, 255, 255), lineType=cv2.LINE_4)
 
-    plt.show()
+cv2.imshow('Result', result)
+cv2.waitKey()
+
+
