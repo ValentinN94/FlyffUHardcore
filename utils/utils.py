7014149709115d2addb132f50f9b43b28d394780
ValentinN94 <negruvalentin94@yahoo.com>
2023-11-01 20:48:40 +0200

    utils and images updated with death and buff_pang needle

--- utils/utils.py
+++ utils/utils.py
@@ -293,6 +293,80 @@ import cv2
 
 # Windows capture test
 ########################################################################################################################
+# import os
+# from time import time
+# from windowcapture import WindowCapture
+# from src.detection.vision import Vision
+# from PyQt5.QtCore import QObject, pyqtSignal
+# import cv2 as cv
+#
+# script_directory = os.path.dirname(os.path.abspath(__file__))
+# src_directory = os.path.join(script_directory, os.path.pardir)
+# main_directory = os.path.join(src_directory, os.path.pardir)
+# os.chdir(main_directory)
+# os.chdir('FlyffUHardcore')
+# image_path = os.path.join(os.getcwd(), 'images', 'char_status', 'LVL_target.PNG')
+# print(f"Full image path: {image_path}")
+#
+# wincap = WindowCapture('PycharmProjects')
+# new_char = Vision(image_path)
+#
+# loop_time = time()
+# wincap.start()
+# counter = 0
+# while True:
+#     if wincap.screenshot is None:
+#         continue
+#
+#     points = new_char.find(wincap.screenshot)
+#
+#     print('FPS {}'.format(1 / (time() - loop_time)))
+#     loop_time = time()
+#     print(counter)
+#     cv.imwrite('result' + str(counter) +'.jpg', wincap.screenshot)
+#     counter += 1
+#     if points:
+#        print('Pass')
+
+#Testing of death matching
+#######################################################################################################################
+# import os
+# from time import time
+# from windowcapture import WindowCapture
+# from src.detection.vision import Vision
+# from PyQt5.QtCore import QObject, pyqtSignal
+# import cv2 as cv
+#
+# script_directory = os.path.dirname(os.path.abspath(__file__))
+# src_directory = os.path.join(script_directory, os.path.pardir)
+# main_directory = os.path.join(src_directory, os.path.pardir)
+# os.chdir(main_directory)
+# os.chdir('FlyffUHardcore')
+# image_path = os.path.join(os.getcwd(), 'images', 'death', 'resurrection.PNG')
+# print(f"Full image path: {image_path}")
+#
+# wincap = WindowCapture('FlyffHarcore')
+# new_char = Vision(image_path)
+#
+# loop_time = time()
+# wincap.start()
+# counter = 0
+# while True:
+#     if wincap.screenshot is None:
+#         continue
+#
+#     points = new_char.find(wincap.screenshot)
+#
+#     print('FPS {}'.format(1 / (time() - loop_time)))
+#     loop_time = time()
+#     print(counter)
+#     cv.imwrite('result' + str(counter) +'.jpg', wincap.screenshot)
+#     counter += 1
+#     if points:
+#        print('Pass')
+
+#Testing of buff pang
+#######################################################################################################################
 import os
 from time import time
 from windowcapture import WindowCapture
@@ -305,10 +379,10 @@ src_directory = os.path.join(script_directory, os.path.pardir)
 main_directory = os.path.join(src_directory, os.path.pardir)
 os.chdir(main_directory)
 os.chdir('FlyffUHardcore')
-image_path = os.path.join(os.getcwd(), 'images', 'char_status', 'LVL_target.PNG')
+image_path = os.path.join(os.getcwd(), 'images', 'buff_pang', 'buff_pang_dialog.PNG')
 print(f"Full image path: {image_path}")
 
-wincap = WindowCapture('PycharmProjects')
+wincap = WindowCapture('FlyffHarcore')
 new_char = Vision(image_path)
 
 loop_time = time()
